dafee9336fc6b1f9161c54d054a41049e5a62f57
Daniel Duque <daniel.duque@cern.ch>
2022-06-05 22:51:31 -0700

    Add try_from<&[u8]> for AdcV3Packet

--- detector/src/alpha16.rs
+++ detector/src/alpha16.rs
@@ -261,55 +261,82 @@ impl BoardId {
 /// [`AdcPacket`] fails.
 #[derive(Clone, Copy, Debug)]
 pub enum TryAdcPacketFromSliceError {
+    /// The input slice is not long enough to contain a complete packet.
+    IncompleteSlice,
     /// Unknown packet type. This value should currently be fixed to `1`.
     UnknownType,
     /// Unknown packet version. This value should currently be fixed to `3`.
     UnknownVersion,
-    /// Integer representation of Module ID doesn't match any known Alpha16.
-    /// board.
+    /// Integer representation of Module ID doesn't match any known
+    /// [`ModuleId`].
     UnknownModuleId,
-    /// Unknown channel in the Alpha16 board.
+    /// Integer representation of channel ID doesn't match any known
+    /// [`ChannelId`].
     UnknownChannelId,
-    /// Non-zero value found in bytes meant to be fixed to 0.
+    /// Non-zero value found in bytes meant to be fixed to `0`.
     ZeroMismatch,
-    /// MAC address doesn't match any known Alpha16 board.
+    /// MAC address doesn't map to any known [`BoardId`].
     UnknownMac,
     /// Suppression baseline in the footer doesn't match waveform samples.
     BaselineMismatch,
     /// The number of waveform samples is less than the number indicated by
     /// `keep_last` in the footer.
+    // The `keep_more` and `threshold` values are not known here, so a more
+    // specific error than this is not possible.
     BadKeepLast,
     /// The `keep_bit` in the footer is set, but there are no waveform samples.
     /// Or `keep_bit` is not set, but there are waveform samples.
+    // The `threshold` is not known here, so a more specific error than this is
+    // not possible.
     KeepBitMismatch,
     /// Data suppression is not enabled, and the number of waveform samples
-    /// doesn't match the number of requested samples.
-    RequestedSamplesMismatch,
-    // Is there a failure mode from number of samples requested? e.g. there are
-    // more samples than the requested number.
+    /// doesn't match the number of requested samples. Or data suppression is
+    /// enabled, and the number of waveform samples is greater than the number
+    /// of requested samples.
+    // The `keep_more` is not known here, so a more specific error than this is
+    // not possible.
+    NumberOfSamplesMismatch,
+    /// The number of waveform bytes is not even; recall that each waveform
+    /// sample is [`i16`]. Or the number of waveform samples is shorted than the
+    /// minimum required to reproduce the suppression baseline.
+    IncompleteWaveform,
 }
 impl fmt::Display for TryAdcPacketFromSliceError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            TryAdcPacketFromSliceError::IncompleteSlice => write!(f, "short input slice"),
             TryAdcPacketFromSliceError::UnknownType => write!(f, "unknown packet type"),
             TryAdcPacketFromSliceError::UnknownVersion => write!(f, "unknown packet version"),
             TryAdcPacketFromSliceError::UnknownModuleId => write!(f, "unknown module id"),
             TryAdcPacketFromSliceError::UnknownChannelId => write!(f, "unknown channel number"),
             TryAdcPacketFromSliceError::ZeroMismatch => write!(f, "zero-bytes mismatch"),
             TryAdcPacketFromSliceError::UnknownMac => write!(f, "unknown mac address"),
-            TryAdcPacketFromSliceError::BaselineMismatch => write!(f, "baseline mismatch"),
+            TryAdcPacketFromSliceError::BaselineMismatch => {
+                write!(f, "suppression baseline mismatch")
+            }
             TryAdcPacketFromSliceError::BadKeepLast => write!(f, "bad keep_last"),
             TryAdcPacketFromSliceError::KeepBitMismatch => write!(f, "keep_bit mismatch"),
-            TryAdcPacketFromSliceError::RequestedSamplesMismatch => {
-                write!(f, "requested number of samples mismatch")
+            TryAdcPacketFromSliceError::NumberOfSamplesMismatch => {
+                write!(f, "number of samples mismatch")
             }
+            TryAdcPacketFromSliceError::IncompleteWaveform => write!(f, "incomplete waveform"),
         }
     }
 }
 impl Error for TryAdcPacketFromSliceError {}
+impl From<TryModuleIdFromUnsignedError> for TryAdcPacketFromSliceError {
+    fn from(_: TryModuleIdFromUnsignedError) -> Self {
+        Self::UnknownModuleId
+    }
+}
 impl From<TryChannelIdFromUnsignedError> for TryAdcPacketFromSliceError {
     fn from(_: TryChannelIdFromUnsignedError) -> Self {
         Self::UnknownChannelId
+    }
+}
+impl From<TryBoardIdFromMacAddressError> for TryAdcPacketFromSliceError {
+    fn from(_: TryBoardIdFromMacAddressError) -> Self {
+        Self::UnknownMac
     }
 }
 
@@ -318,12 +345,14 @@ pub struct AdcV3Packet {
     accepted_trigger: u16,
     module_id: ModuleId,
     channel_id: ChannelId,
-    samples_requested: usize,
+    requested_samples: usize,
     event_timestamp: u64,
     board_id: Option<BoardId>,
-    trigger_offset: Option<u32>,
+    trigger_offset: Option<i32>,
     build_timestamp: Option<u32>,
     waveform: Vec<i16>,
+    // Only `keep_bit` and `baseline` can be deduced by logic from the other
+    // fields. Both `keep_last` and `suppression_enabled` need to be stored.
     keep_last: usize,
     suppression_enabled: bool,
 }
@@ -344,16 +373,16 @@ impl AdcV3Packet {
     pub fn channel_id(&self) -> ChannelId {
         self.channel_id
     }
-    pub fn samples_requested(&self) -> usize {
-        self.samples_requested
+    pub fn requested_samples(&self) -> usize {
+        self.requested_samples
     }
     pub fn event_timestamp(&self) -> u64 {
         self.event_timestamp
     }
-    pub fn mac_address(&self) -> Option<[u8; 6]> {
-        self.board_id.map(|board| board.mac_address)
+    pub fn board_id(&self) -> Option<BoardId> {
+        self.board_id
     }
-    pub fn trigger_offset(&self) -> Option<u32> {
+    pub fn trigger_offset(&self) -> Option<i32> {
         self.trigger_offset
     }
     pub fn build_timestamp(&self) -> Option<u32> {
@@ -362,14 +391,143 @@ impl AdcV3Packet {
     pub fn waveform(&self) -> &[i16] {
         &self.waveform
     }
-    pub fn baseline(&self) -> i16 {
-        todo!()
+    pub fn suppression_baseline(&self) -> Option<i16> {
+        if self.waveform.is_empty() {
+            None
+        } else {
+            // Add over i32 because adding 64 i16s can overflow.
+            Some(
+                i16::try_from(
+                    self.waveform[..64]
+                        .iter()
+                        .map(|n| i32::from(*n))
+                        .sum::<i32>()
+                        / 64,
+                )
+                .unwrap(),
+            )
+        }
+    }
+    pub fn keep_last(&self) -> usize {
+        self.keep_last
     }
     pub fn is_suppression_enabled(&self) -> bool {
         self.suppression_enabled
     }
-    pub fn keep_last(&self) -> usize {
-        self.keep_last
+}
+
+impl TryFrom<&[u8]> for AdcV3Packet {
+    type Error = TryAdcPacketFromSliceError;
+
+    // All fields are big endian
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        if slice.len() < 16 {
+            return Err(Self::Error::IncompleteSlice);
+        }
+
+        if slice[0] != 1 {
+            return Err(Self::Error::UnknownType);
+        }
+        if slice[1] != 3 {
+            return Err(Self::Error::UnknownVersion);
+        }
+        let accepted_trigger = slice[2..4].try_into().unwrap();
+        let accepted_trigger = u16::from_be_bytes(accepted_trigger);
+        let module_id = ModuleId::try_from(slice[4])?;
+        let channel_id = slice[5];
+        let channel_id = if channel_id < 128 {
+            ChannelId::A16(channel_id.try_into()?)
+        } else {
+            ChannelId::A32((channel_id - 128).try_into()?)
+        };
+        let requested_samples = slice[6..8].try_into().unwrap();
+        let requested_samples = u16::from_be_bytes(requested_samples).into();
+        let lsw_event_timestamp = slice[8..12].try_into().unwrap();
+
+        let baseline = slice[slice.len() - 2..].try_into().unwrap();
+        let baseline = i16::from_be_bytes(baseline);
+        let footer = slice[slice.len() - 4..][..2].try_into().unwrap();
+        let footer = u16::from_be_bytes(footer);
+        let keep_last = usize::from(footer & 0xFFF);
+        let keep_bit = (footer >> 12) & 1 == 1;
+        let suppression_enabled = (footer >> 13) & 1 == 1;
+
+        if slice.len() == 16 {
+            if keep_bit {
+                return Err(Self::Error::KeepBitMismatch);
+            }
+            return Ok(AdcV3Packet {
+                accepted_trigger,
+                module_id,
+                channel_id,
+                requested_samples,
+                event_timestamp: u32::from_be_bytes(lsw_event_timestamp).into(),
+                board_id: None,
+                trigger_offset: None,
+                build_timestamp: None,
+                waveform: Vec::new(),
+                keep_last,
+                suppression_enabled,
+            });
+        }
+
+        if slice.len() < 36 {
+            return Err(Self::Error::IncompleteSlice);
+        }
+
+        if slice[12..14] != [0, 0] {
+            return Err(Self::Error::ZeroMismatch);
+        }
+        let board_id: [u8; 6] = slice[14..20].try_into().unwrap();
+        let board_id = BoardId::try_from(board_id)?;
+        let msw_event_timestamp = slice[20..24].try_into().unwrap();
+        let event_timestamp = [msw_event_timestamp, lsw_event_timestamp].concat();
+        let event_timestamp = event_timestamp.try_into().unwrap();
+        let event_timestamp = u64::from_be_bytes(event_timestamp);
+        let trigger_offset = slice[24..28].try_into().unwrap();
+        let trigger_offset = i32::from_be_bytes(trigger_offset);
+        let build_timestamp = slice[28..32].try_into().unwrap();
+        let build_timestamp = u32::from_be_bytes(build_timestamp);
+
+        let waveform_bytes = slice.len() - 36;
+        if waveform_bytes % 2 != 0 || waveform_bytes < 128 {
+            return Err(Self::Error::IncompleteWaveform);
+        }
+        if !keep_bit {
+            return Err(Self::Error::KeepBitMismatch);
+        }
+        if suppression_enabled && waveform_bytes / 2 > requested_samples {
+            return Err(Self::Error::NumberOfSamplesMismatch);
+        }
+        if !suppression_enabled && waveform_bytes / 2 != requested_samples {
+            return Err(Self::Error::NumberOfSamplesMismatch);
+        }
+        if waveform_bytes / 2 + 3 < 2 * keep_last {
+            return Err(Self::Error::BadKeepLast);
+        }
+
+        let waveform: Vec<i16> = slice[32..][..waveform_bytes]
+            .chunks_exact(2)
+            .map(|b| i16::from_be_bytes(b.try_into().unwrap()))
+            .collect();
+
+        if i32::from(baseline) != waveform[..64].iter().map(|n| i32::from(*n)).sum::<i32>() / 64 {
+            return Err(Self::Error::BaselineMismatch);
+        }
+
+        Ok(AdcV3Packet {
+            accepted_trigger,
+            module_id,
+            channel_id,
+            requested_samples,
+            event_timestamp,
+            board_id: Some(board_id),
+            trigger_offset: Some(trigger_offset),
+            build_timestamp: Some(build_timestamp),
+            waveform,
+            keep_last,
+            suppression_enabled,
+        })
     }
 }
 
