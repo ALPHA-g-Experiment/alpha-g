3702816a88742fe76d1cd3a80c33bf7fadf71608
Daniel Duque <daniel.duque@cern.ch>
2022-07-15 21:35:35 -0700

    Add Adc16BankName

--- detector/src/midas.rs
+++ detector/src/midas.rs
@@ -1,7 +1,9 @@
+use crate::alpha16::{Adc16ChannelId, BoardId, ParseBoardIdError, TryChannelIdFromUnsignedError};
+use std::num::ParseIntError;
 use std::{error::Error, fmt};
 
 /// The error type returned when conversion from unsigned integer to [`EventId`]
-/// fails,
+/// fails.
 #[derive(Clone, Copy, Debug)]
 pub struct TryEventIdFromUnsignedError;
 impl fmt::Display for TryEventIdFromUnsignedError {
@@ -27,6 +29,68 @@ impl TryFrom<u16> for EventId {
             1 => Ok(EventId::Main),
             _ => Err(TryEventIdFromUnsignedError),
         }
+    }
+}
+
+/// The error type returned when parsing an Alpha16 bank name fails.
+#[derive(Clone, Copy, Debug)]
+pub enum ParseAlpha16BankNameError {
+    /// Input string pattern doesn't match expected Alpha16 bank name pattern.
+    PatternMismatch,
+    /// Board name doesn't match any known [`BoardId`].
+    UnknownBoardId,
+    /// Channel ID doesn't match any known [`ChannelId`].
+    UnknownChannelId,
+}
+impl fmt::Display for ParseAlpha16BankNameError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::PatternMismatch => write!(f, "pattern mismatch"),
+            Self::UnknownBoardId => write!(f, "unknown board id"),
+            Self::UnknownChannelId => write!(f, "unknown channel id"),
+        }
+    }
+}
+impl Error for ParseAlpha16BankNameError {}
+impl From<ParseBoardIdError> for ParseAlpha16BankNameError {
+    fn from(_: ParseBoardIdError) -> Self {
+        Self::UnknownBoardId
+    }
+}
+impl From<TryChannelIdFromUnsignedError> for ParseAlpha16BankNameError {
+    fn from(_: TryChannelIdFromUnsignedError) -> Self {
+        Self::UnknownChannelId
+    }
+}
+impl From<ParseIntError> for ParseAlpha16BankNameError {
+    fn from(_: ParseIntError) -> Self {
+        Self::UnknownChannelId
+    }
+}
+
+/// Bank name that corresponds to SiPMs of the Barrel Veto.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Adc16BankName {
+    pub board_id: BoardId,
+    pub channel_id: Adc16ChannelId,
+}
+impl TryFrom<&str> for Adc16BankName {
+    type Error = ParseAlpha16BankNameError;
+
+    fn try_from(name: &str) -> Result<Self, Self::Error> {
+        if !name.starts_with('B')
+            || name.len() != 4
+            || !name.chars().all(|c| c.is_ascii_alphanumeric())
+            || name.chars().any(|c| c.is_ascii_lowercase())
+        {
+            return Err(Self::Error::PatternMismatch);
+        }
+        let board_id = BoardId::try_from(&name[1..][..2])?;
+        let channel_id = Adc16ChannelId::try_from(u8::from_str_radix(&name[3..], 16)?)?;
+        Ok(Adc16BankName {
+            board_id,
+            channel_id,
+        })
     }
 }
 
--- detector/src/midas/tests.rs
+++ detector/src/midas/tests.rs
@@ -10,3 +10,96 @@ fn event_id_try_from_u16() {
         }
     }
 }
+
+#[test]
+fn adc_16_bank_name_pattern_mismatch() {
+    assert!(matches!(
+        Adc16BankName::try_from("C09A"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("B91"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("B0911"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("B0¹"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("B09 "),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("B09a"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+    assert!(matches!(
+        Adc16BankName::try_from("b09A"),
+        Err(ParseAlpha16BankNameError::PatternMismatch)
+    ));
+}
+
+#[test]
+fn adc_16_bank_name_unknown_board_id() {
+    for num in 0..9 {
+        let name = format!("B{num:0>2}0");
+        assert!(matches!(
+            Adc16BankName::try_from(&name[..]),
+            Err(ParseAlpha16BankNameError::UnknownBoardId)
+        ));
+    }
+    for num in 19..100 {
+        let name = format!("B{num}0");
+        assert!(matches!(
+            Adc16BankName::try_from(&name[..]),
+            Err(ParseAlpha16BankNameError::UnknownBoardId)
+        ));
+    }
+}
+
+#[test]
+fn adc_16_bank_name_unknown_channel_id() {
+    for chan in 'G'..='Z' {
+        let name = format!("B09{chan}");
+        assert!(matches!(
+            Adc16BankName::try_from(&name[..]),
+            Err(ParseAlpha16BankNameError::UnknownChannelId)
+        ));
+    }
+}
+
+#[test]
+fn valid_adc_16_bank_name() {
+    for num in 9..=14 {
+        for chan in 0..=9 {
+            let bank_name = format!("B{num:0>2}{chan}");
+            let bank_name = Adc16BankName::try_from(&bank_name[..]).unwrap();
+            assert_eq!(
+                bank_name.board_id,
+                BoardId::try_from(&format!("{num:0>2}")[..]).unwrap()
+            );
+            assert_eq!(
+                bank_name.channel_id,
+                Adc16ChannelId::try_from(chan).unwrap()
+            );
+        }
+    }
+    for num in 9..=14 {
+        for (i, chan) in ('A'..='F').into_iter().enumerate() {
+            let bank_name = format!("B{num:0>2}{chan}");
+            let bank_name = Adc16BankName::try_from(&bank_name[..]).unwrap();
+            assert_eq!(
+                bank_name.board_id,
+                BoardId::try_from(&format!("{num:0>2}")[..]).unwrap()
+            );
+            assert_eq!(
+                bank_name.channel_id,
+                Adc16ChannelId::try_from(u8::try_from(i).unwrap() + 10).unwrap()
+            );
+        }
+    }
+}
