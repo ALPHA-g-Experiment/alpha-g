c426d0900693af9aa5780f2b25c779d8b6b6fa15
Daniel Duque <daniel.duque@cern.ch>
2023-01-27 22:19:09 -0800

    Use PartialEq trait for Filter comparisons. Fix issue #55

--- analysis/src/bin/alpha-g-alpha16-signal-viewer/filter/tests.rs
+++ analysis/src/bin/alpha-g-alpha16-signal-viewer/filter/tests.rs
@@ -138,130 +138,139 @@ fn packet_overflow() {
 
 #[test]
 fn packet_passes_correctness_filter() {
-    let filter = Filter {
+    let filter_one = Filter {
         correctness: Some(Correctness::Good),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_one = Packet {
         adc_packet: LONG_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_one.passes_filter(&filter_one));
 
-    let filter = Filter {
+    let filter_two = Filter {
         correctness: Some(Correctness::Bad),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_two = Packet {
         adc_packet: vec![0],
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_two.passes_filter(&filter_two));
+
+    assert!(!packet_one.passes_filter(&filter_two));
+    assert!(!packet_two.passes_filter(&filter_one));
 }
 
 #[test]
 fn packet_passes_detector_filter() {
-    let filter = Filter {
+    let filter_one = Filter {
         detector: Some(Detector::Tpc),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_one = Packet {
         adc_packet: LONG_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("C09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_one.passes_filter(&filter_one));
 
-    let filter = Filter {
+    let filter_two = Filter {
         detector: Some(Detector::Bv),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_two = Packet {
         adc_packet: LONG_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_two.passes_filter(&filter_two));
+
+    assert!(!packet_one.passes_filter(&filter_two));
+    assert!(!packet_two.passes_filter(&filter_one));
 }
 
 #[test]
 fn packet_passes_keep_bit_filter() {
-    let filter = Filter {
+    let filter_one = Filter {
         keep_bit: Some(true),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_one = Packet {
         adc_packet: LONG_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("C09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_one.passes_filter(&filter_one));
 
-    let filter = Filter {
+    let filter_two = Filter {
         keep_bit: Some(false),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_two = Packet {
         adc_packet: SHORT_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("C09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_two.passes_filter(&filter_two));
+
+    assert!(!packet_one.passes_filter(&filter_two));
+    assert!(!packet_two.passes_filter(&filter_one));
 }
 
 #[test]
 fn packet_passes_overflow_filter() {
-    let filter = Filter {
+    let filter_one = Filter {
         overflow: Some(Overflow::Neither),
         ..Filter::default()
     };
-    let packet = Packet {
+    let packet_one = Packet {
         adc_packet: LONG_ADC_V3_PACKET.to_vec(),
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_one.passes_filter(&filter_one));
 
-    let filter = Filter {
+    let filter_two = Filter {
         overflow: Some(Overflow::Negative),
         ..Filter::default()
     };
     let mut adc_packet = LONG_ADC_V3_PACKET.to_vec();
     adc_packet[160] = 128;
     adc_packet[161] = 0;
-    let packet = Packet {
+    let packet_two = Packet {
         adc_packet,
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_two.passes_filter(&filter_two));
 
-    let filter = Filter {
+    let filter_three = Filter {
         overflow: Some(Overflow::Positive),
         ..Filter::default()
     };
     let mut adc_packet = LONG_ADC_V3_PACKET.to_vec();
     adc_packet[160] = 127;
     adc_packet[161] = 252;
-    let packet = Packet {
+    let packet_three = Packet {
         adc_packet,
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_three.passes_filter(&filter_three));
 
-    let filter = Filter {
+    let filter_four = Filter {
         overflow: Some(Overflow::Both),
         ..Filter::default()
     };
@@ -270,11 +279,24 @@ fn packet_passes_overflow_filter() {
     adc_packet[161] = 252;
     adc_packet.insert(160, 128);
     adc_packet.insert(161, 0);
-    let packet = Packet {
+    let packet_four = Packet {
         adc_packet,
         bank_name: String::from("B09A"),
         a16_suppression: Some(500.0),
         a32_suppression: Some(1500.0),
     };
-    assert!(packet.passes_filter(&filter));
+    assert!(packet_four.passes_filter(&filter_four));
+
+    assert!(!packet_one.passes_filter(&filter_two));
+    assert!(!packet_one.passes_filter(&filter_three));
+    assert!(!packet_one.passes_filter(&filter_four));
+    assert!(!packet_two.passes_filter(&filter_one));
+    assert!(!packet_two.passes_filter(&filter_three));
+    assert!(!packet_two.passes_filter(&filter_four));
+    assert!(!packet_three.passes_filter(&filter_one));
+    assert!(!packet_three.passes_filter(&filter_two));
+    assert!(!packet_three.passes_filter(&filter_four));
+    assert!(!packet_four.passes_filter(&filter_one));
+    assert!(!packet_four.passes_filter(&filter_two));
+    assert!(!packet_four.passes_filter(&filter_three));
 }
