f45f8e570ded1a9c9e4fa2779e7e07519d31a6c0
Daniel Duque <daniel.duque@cern.ch>
2023-04-03 18:31:48 -0700

    Implement cumulative distribution

--- analysis/src/bin/alpha-g-wire-gain-calibration/distribution.rs
+++ analysis/src/bin/alpha-g-wire-gain-calibration/distribution.rs
@@ -1,5 +1,6 @@
 use alpha_g_detector::alpha16::{ADC_MAX, ADC_MIN};
 
+#[derive(Clone, Debug)]
 pub(crate) struct Distribution {
     samples: Vec<usize>,
 }
@@ -28,25 +29,109 @@ impl Distribution {
         self.samples[index] += multiplicity;
     }
     /// Re-scale the samples with a given factor.
-    pub(crate) fn rescale(&self, factor: f64) -> Self {
+    pub(crate) fn rescale(self, factor: f64) -> Self {
         // Sample that corresponds to index 0
         let min = i32::from(ADC_MIN) - i32::from(ADC_MAX);
 
         let mut scaled = Self::new();
         for (index, count) in self
             .samples
-            .iter()
+            .into_iter()
             .enumerate()
-            .filter(|(_, count)| **count > 0)
+            .filter(|(_, count)| *count > 0)
         {
             let sample = min + index as i32;
             let scaled_sample = f64::from(sample) * factor;
 
-            // Add `count` samples to the scaled distribution
-            scaled.add_sample(scaled_sample.round() as i32, *count);
+            scaled.add_sample(scaled_sample.round() as i32, count);
         }
 
         scaled
+    }
+    /// Saturate the distribution at a given threshold.
+    /// All samples greater than the `positive_threshold` will be set to
+    /// `positive_threshold`.
+    /// All samples less than the `negative_threshold` will be set to
+    /// `negative_threshold`.
+    pub(crate) fn saturate(self, negative_threshold: i32, positive_threshold: i32) -> Self {
+        // Sample that corresponds to index 0
+        let min = i32::from(ADC_MIN) - i32::from(ADC_MAX);
+
+        let mut saturated = Self::new();
+        for (index, count) in self
+            .samples
+            .into_iter()
+            .enumerate()
+            .filter(|(_, count)| *count > 0)
+        {
+            let sample = min + index as i32;
+            let sample = sample.clamp(negative_threshold, positive_threshold);
+
+            saturated.add_sample(sample, count);
+        }
+        saturated
+    }
+    /// Suppress the distribution at a given threshold.
+    /// All samples between -`threshold` and +`threshold` (inclusive) will be
+    /// suppressed.
+    pub(crate) fn suppress(self, threshold: i32) -> Self {
+        // Sample that corresponds to index 0
+        let min = i32::from(ADC_MIN) - i32::from(ADC_MAX);
+
+        let threshold = threshold.abs();
+        let mut suppressed = Self::new();
+        for (index, count) in self
+            .samples
+            .into_iter()
+            .enumerate()
+            .filter(|(_, count)| *count > 0)
+        {
+            let sample = min + index as i32;
+            if sample.abs() <= threshold {
+                continue;
+            }
+
+            suppressed.add_sample(sample, count);
+        }
+        suppressed
+    }
+}
+
+pub(crate) struct CumulativeDistribution {
+    samples: Vec<f64>,
+}
+
+impl CumulativeDistribution {
+    /// Create a cumulative distribution from a distribution.
+    // The distribution only has raw counts, so we need to first accumulate
+    // the counts, and then normalize.
+    pub(crate) fn from_distribution(distribution: &Distribution) -> Self {
+        let mut samples = Vec::with_capacity(distribution.samples.len());
+        let mut sum = 0;
+        for count in distribution.samples.iter() {
+            sum += count;
+            samples.push(sum);
+        }
+
+        let total = samples.last().copied().unwrap_or(0);
+        let samples = samples
+            .into_iter()
+            .map(|count| count as f64 / total as f64)
+            .collect();
+
+        Self { samples }
+    }
+    /// Calculate the Kolmogorov-Smirnov distance between two cumulative
+    /// distributions.
+    pub(crate) fn ks_distance(&self, other: &Self) -> f64 {
+        let mut max_distance = 0.0;
+        for (self_sample, other_sample) in self.samples.iter().zip(other.samples.iter()) {
+            let distance = (self_sample - other_sample).abs();
+            if distance > max_distance {
+                max_distance = distance;
+            }
+        }
+        max_distance
     }
 }
 
