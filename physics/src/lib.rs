7e399acc879fea40fd7991fbe3fc94fb106d9f26
Daniel Duque <daniel.duque@cern.ch>
2023-06-04 20:44:37 -0700

    Unpack the cathode pad signals into MainEvent

--- physics/src/lib.rs
+++ physics/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::calibration::pads::baseline::try_pad_baseline;
+use crate::calibration::pads::gain::try_pad_gain;
 use crate::calibration::wires::baseline::try_wire_baseline;
 use crate::calibration::wires::gain::try_wire_gain;
 use alpha_g_detector::alpha16::aw_map::{
@@ -7,9 +9,14 @@ use alpha_g_detector::alpha16::{self, AdcPacket, TryAdcPacketFromSliceError};
 use alpha_g_detector::midas::{
     Adc32BankName, Alpha16BankName, MainEventBankName, ParseMainEventBankNameError,
 };
-use alpha_g_detector::padwing::map::MapTpcPadPositionError;
-use alpha_g_detector::padwing::{TryChunkFromSliceError, TryPwbPacketFromChunksError};
+use alpha_g_detector::padwing::map::{
+    MapTpcPadPositionError, TpcPadPosition, TPC_PAD_COLUMNS, TPC_PAD_ROWS,
+};
+use alpha_g_detector::padwing::{
+    self, Chunk, PwbPacket, TryChunkFromSliceError, TryPwbPacketFromChunksError,
+};
 use alpha_g_detector::trigger::TryTrgPacketFromSliceError;
+use std::collections::HashMap;
 use thiserror::Error;
 
 pub use crate::calibration::pads::baseline::MapPadBaselineError;
@@ -39,18 +46,35 @@ pub enum TryMainEventFromDataBanksError {
     /// The data from an Alpha16 board is invalid.
     #[error("bad alpha16 data")]
     BadAlpha16(#[from] TryAdcPacketFromSliceError),
+    /// The `(board_id, channel_id)` pair in an ADC packet does not match the
+    /// expected value from the bank name.
+    #[error("alpha16 board_id/channel_id mismatch (expected {expected:?}, found {found:?})")]
+    Alpha16IdMismatch {
+        expected: (alpha16::BoardId, alpha16::Adc32ChannelId),
+        found: (alpha16::BoardId, alpha16::Adc32ChannelId),
+    },
     /// An anode wire data bank has a BV channel_id.
     #[error("anode wire data bank `{bank_name:?}` has a BV channel_id")]
     WireBankWithBvChannel { bank_name: Adc32BankName },
     /// Duplicate anode wire data banks were found.
-    #[error("duplicate anode wire data banks `{bank_name:?}`")]
+    #[error("duplicate anode wire data banks with name `{bank_name:?}`")]
     DuplicateWireBank { bank_name: Adc32BankName },
     /// The chunk data from a Padwing bank is invalid.
     #[error("bad padwing chunk data")]
     BadPadwingChunk(#[from] TryChunkFromSliceError),
+    /// The Padwing board_id in a chunk does not match the expected value from
+    /// the bank name.
+    #[error("padwing board_id mismatch (expected `{expected:?}`, found `{found:?}`)")]
+    PadwingBoardIdMismatch {
+        expected: padwing::BoardId,
+        found: padwing::BoardId,
+    },
     /// The chunks from a Padwing board are invalid.
     #[error("bad padwing data")]
     BadPadwing(#[from] TryPwbPacketFromChunksError),
+    /// Duplicate cathode pad signal found.
+    #[error("duplicate cathode pad signal in position `{position:?}`")]
+    DuplicatePadSignal { position: TpcPadPosition },
     /// The data from the TRG board is invalid.
     #[error("bad trg data")]
     BadTrg(#[from] TryTrgPacketFromSliceError),
@@ -78,6 +102,7 @@ pub enum TryMainEventFromDataBanksError {
 #[derive(Debug, Clone)]
 pub struct MainEvent {
     wire_signals: [Option<Vec<f64>>; TPC_ANODE_WIRES],
+    pad_signals: [[Option<Vec<f64>>; TPC_PAD_ROWS]; TPC_PAD_COLUMNS],
 }
 impl MainEvent {
     /// Given a run number, try to convert data banks to a [`MainEvent`]. The
@@ -90,7 +115,11 @@ impl MainEvent {
     where
         I: IntoIterator<Item = (&'a str, &'a [u8])>,
     {
+        // I didn't find another way to initialize such large arrays.
         let mut wire_signals = [(); TPC_ANODE_WIRES].map(|_| None);
+        let mut pad_signals = [(); TPC_PAD_COLUMNS].map(|_| [(); TPC_PAD_ROWS].map(|_| None));
+        // Need to group chunks by board and chip.
+        let mut pwb_chunks_map: HashMap<_, Vec<_>> = HashMap::new();
 
         for (bank_name, data_slice) in banks {
             match MainEventBankName::try_from(bank_name)? {
@@ -105,14 +134,21 @@ impl MainEvent {
                     let board_id = packet.board_id().unwrap();
                     let alpha16::ChannelId::A32(channel_id) = packet.channel_id() else {
                         return Err(TryMainEventFromDataBanksError::WireBankWithBvChannel {
-                            bank_name: bank_name,
+                            bank_name,
                         });
                     };
+                    if (bank_name.board_id(), bank_name.channel_id()) != (board_id, channel_id) {
+                        return Err(TryMainEventFromDataBanksError::Alpha16IdMismatch {
+                            expected: (bank_name.board_id(), bank_name.channel_id()),
+                            found: (board_id, channel_id),
+                        });
+                    }
+
                     let wire_position = TpcWirePosition::try_new(run_number, board_id, channel_id)?;
                     let wire_index = usize::from(wire_position);
                     if wire_signals[wire_index].is_some() {
                         return Err(TryMainEventFromDataBanksError::DuplicateWireBank {
-                            bank_name: bank_name,
+                            bank_name,
                         });
                     } else {
                         let baseline = try_wire_baseline(run_number, wire_position)?;
@@ -120,15 +156,67 @@ impl MainEvent {
                         wire_signals[wire_index] = Some(
                             waveform
                                 .iter()
-                                .map(|&v| f64::from(v - baseline) * gain)
+                                // Convert to i32 to avoid overflow
+                                .map(|&v| f64::from(i32::from(v) - i32::from(baseline)) * gain)
                                 .collect(),
                         );
                     }
+                }
+                MainEventBankName::Padwing(bank_name) => {
+                    let chunk = Chunk::try_from(data_slice)?;
+                    let key = (chunk.board_id(), chunk.after_id());
+                    if key.0 != bank_name.board_id() {
+                        return Err(TryMainEventFromDataBanksError::PadwingBoardIdMismatch {
+                            expected: bank_name.board_id(),
+                            found: key.0,
+                        });
+                    }
+
+                    pwb_chunks_map.entry(key).or_default().push(chunk);
                 }
                 _ => {}
             }
         }
 
-        Ok(Self { wire_signals })
+        for chunks in pwb_chunks_map.into_values() {
+            let packet = PwbPacket::try_from(chunks)?;
+            let board_id = packet.board_id();
+            let after_id = packet.after_id();
+            for &channel_id in packet.channels_sent() {
+                if let padwing::ChannelId::Pad(pad_channel_id) = channel_id {
+                    // A waveform is guaranteed to exist and not be empty if the
+                    // channel was sent. It is safe to unwrap.
+                    let waveform = packet.waveform_at(channel_id).unwrap();
+
+                    let pad_position =
+                        TpcPadPosition::try_new(run_number, board_id, after_id, pad_channel_id)?;
+                    let pad_index = (
+                        usize::from(pad_position.column),
+                        usize::from(pad_position.row),
+                    );
+                    if pad_signals[pad_index.0][pad_index.1].is_some() {
+                        return Err(TryMainEventFromDataBanksError::DuplicatePadSignal {
+                            position: pad_position,
+                        });
+                    } else {
+                        let baseline = try_pad_baseline(run_number, pad_position)?;
+                        let gain = try_pad_gain(run_number, pad_position)?;
+                        pad_signals[pad_index.0][pad_index.1] = Some(
+                            waveform
+                                .iter()
+                                // Given the ranges of PWB samples, overflow is
+                                // not possible.
+                                .map(|&v| f64::from(v.checked_sub(baseline).unwrap()) * gain)
+                                .collect(),
+                        );
+                    }
+                }
+            }
+        }
+
+        Ok(Self {
+            wire_signals,
+            pad_signals,
+        })
     }
 }
