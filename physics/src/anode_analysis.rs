fe5c57c31f4f05427fa9c7d2836933e050cda6c1
Daniel Duque <daniel.duque@cern.ch>
2023-06-28 20:42:58 -0700

    Cache the BIG_R_MATRIX and LARGEST_SVD

--- physics/src/anode_analysis.rs
+++ physics/src/anode_analysis.rs
@@ -11,23 +11,52 @@ use lazy_static::lazy_static;
 
 // Width in nanoseconds of each wire signal bin.
 const BIN_WIDTH: usize = (1.0e9 / ADC32_RATE) as usize;
+
 const RESPONSE_BYTES: &[u8] = include_bytes!("../data/simulation/tpc_response/wires.json");
+const SVD_BYTES: &[u8] = include_bytes!("../data/optimization/anode_analysis/largest_svd.json");
+
 lazy_static! {
     // The format of the file is a serialized vector with the response every
     // nanosecond. Need to re-bin (by accumulating) by `BIN_WIDTH`.
     static ref WIRE_RESPONSE: Vec<f64> = {
         let raw: Vec<f64> = serde_json::from_slice(RESPONSE_BYTES).unwrap();
 
-        let mut rebinned = Vec::with_capacity(raw.len() / BIN_WIDTH);
-        for i in 0..raw.len() / BIN_WIDTH {
-            let mut sum = 0.0;
-            for j in 0..BIN_WIDTH {
-                sum += raw[i * BIN_WIDTH + j];
+        raw
+            .chunks_exact(BIN_WIDTH)
+            .map(|chunk| chunk.iter().sum())
+            .collect()
+    };
+    // The "r" matrix (see below) is usually big (400 x 400 ish) and it needs to
+    // be calculated for every cluster of wires. Given the nature of the matrix
+    // (Toeplitz and lower triangular) it is cheaper to calculate it once and
+    // then get a reference to the (smaller) submatrix required for each
+    // cluster.
+    static ref BIG_R_MATRIX: faer_core::Mat<f64> = {
+        // Random large number. Larger than the maximum wire signal length.
+        // The actual maximum is 513 (I think).
+        const MAX_SAMPLES: usize = 600;
+        faer_core::Mat::with_dims(MAX_SAMPLES, MAX_SAMPLES, |i, j| {
+            if i >= j {
+                let diff = i - j;
+                if diff < WIRE_RESPONSE.len() {
+                    WIRE_RESPONSE[diff]
+                } else {
+                    0.0
+                }
+            } else {
+                0.0
             }
-            rebinned.push(sum);
-        }
-
-        rebinned
+        })
+    };
+    // Cache the largest singular value of all submatrices of the BIG_R_MATRIX
+    // It is too expensive to calculate it every time.
+    // LARGEST_SVD[i] corresponds to the value from the matrix of size i x i.
+    // For very large runs, computing this was relatively short.
+    // For short runs, the time it took to calculate the SVD was large compared
+    // to the rest of the analysis, hence I decided to simply load them from a
+    // file.
+    static ref LARGEST_SVD: Vec<f64> = {
+        serde_json::from_slice(SVD_BYTES).unwrap()
     };
 }
 // "Strength" of the signal induced on a neighboring wire.
@@ -44,9 +73,7 @@ const NEIGHBOR_FACTORS: [f64; 5] = [1.0, -0.1275, -0.0365, -0.012, -0.0042];
 //
 // Each of these blocks can be treated independently e.g. analyzed in parallel,
 // etc.
-fn find_contiguous_intervals(
-    wire_signals: &[Option<Vec<f64>>; TPC_ANODE_WIRES],
-) -> Vec<(usize, usize)> {
+fn contiguous_ranges(wire_signals: &[Option<Vec<f64>>; TPC_ANODE_WIRES]) -> Vec<(usize, usize)> {
     let mut ranges = Vec::new();
     let mut start = 0;
     let mut end = 0;
@@ -82,44 +109,6 @@ fn find_contiguous_intervals(
     ranges
 }
 
-// I can express my problem as the matrix equation:
-// Y = R * X * A
-// where:
-// - Y is the observed signals.
-// - R is the response matrix.
-// - X is the unknown/wanted avalanches.
-// - A is an induction coefficients matrix.
-//
-// I just need to create all the matrices and solve for X.
-
-// Create the R matrix for a given size.
-fn r_matrix(n: usize) -> faer_core::Mat<f64> {
-    faer_core::Mat::with_dims(n, n, |i, j| {
-        if i >= j {
-            let diff = i - j;
-            if diff < WIRE_RESPONSE.len() {
-                WIRE_RESPONSE[diff]
-            } else {
-                0.0
-            }
-        } else {
-            0.0
-        }
-    })
-}
-
-// Create the A matrix for a given size.
-fn a_matrix(n: usize) -> faer_core::Mat<f64> {
-    faer_core::Mat::with_dims(n, n, |i, j| {
-        let diff = if i > j { i - j } else { j - i };
-        if diff < NEIGHBOR_FACTORS.len() {
-            NEIGHBOR_FACTORS[diff]
-        } else {
-            0.0
-        }
-    })
-}
-
 // Given a range [first, last), return an iterator over the indices.
 fn range_to_indices(range: (usize, usize)) -> Box<dyn Iterator<Item = usize>> {
     let (first, last) = range;
@@ -140,6 +129,28 @@ fn range_to_len(range: (usize, usize)) -> usize {
     }
 }
 
+// I can express my problem as the matrix equation:
+// Y = R * X * A
+// where:
+// - Y is the observed signals.
+// - R is the response matrix.
+// - X is the unknown/wanted avalanches.
+// - A is an induction coefficients matrix.
+//
+// I just need to create all the matrices and solve for X.
+
+// Create the A matrix for a given size.
+fn a_matrix(n: usize) -> faer_core::Mat<f64> {
+    faer_core::Mat::with_dims(n, n, |i, j| {
+        let diff = if i > j { i - j } else { j - i };
+        if diff < NEIGHBOR_FACTORS.len() {
+            NEIGHBOR_FACTORS[diff]
+        } else {
+            0.0
+        }
+    })
+}
+
 // Find the dimensions of our problem given the range of contiguous signals.
 // The first index is the size of the largest signal in the block.
 // The second index is the number of signals in the block.
@@ -147,16 +158,14 @@ fn problem_dimensions(
     wire_signals: &[Option<Vec<f64>>; TPC_ANODE_WIRES],
     range: (usize, usize),
 ) -> (usize, usize) {
-    let mut max_signal_len = 0;
-    for i in range_to_indices(range) {
-        // It is guaranteed that the signal is `Some`. Safe to unwrap.
-        let signal = wire_signals[i].as_ref().unwrap();
-        if signal.len() > max_signal_len {
-            max_signal_len = signal.len();
-        }
-    }
+    let max_len = range_to_indices(range)
+        // Guaranteed to be Some. Safe to unwrap
+        .map(|i| wire_signals[i].as_ref().unwrap().len())
+        .max()
+        // Guaranteed to not be empty. Safe to unwrap
+        .unwrap();
 
-    (max_signal_len, range_to_len(range))
+    (max_len, range_to_len(range))
 }
 
 // Create the Y matrix for a given range of signals.
@@ -184,7 +193,7 @@ fn solve_x(
     range: (usize, usize),
 ) -> faer_core::Mat<f64> {
     let (i, j) = problem_dimensions(wire_signals, range);
-    let r = r_matrix(i);
+    let r = BIG_R_MATRIX.as_ref().submatrix(0, 0, i, i);
     let mut a = a_matrix(j);
     let mut y = y_matrix(wire_signals, range);
 
@@ -221,13 +230,14 @@ fn solve_x(
         faer_core::Parallelism::None,
         stack.rb_mut(),
     );
+    // At this point I have the system:
+    // Y = R * X
+    //
+    // R has a huge condition number, so the problem is basically ill-formed.
+    // Solve by minimizing |R * X - Y|_2.
+    // Use Landweber iteration.
+    let omega = 2.0 / LARGEST_SVD[i].powi(2);
+    let mut x = faer_core::Mat::zeros(i, j);
 
-    faer_core::solve::solve_lower_triangular_in_place_with_conj(
-        r.as_ref(),
-        faer_core::Conj::No,
-        y.as_mut(),
-        faer_core::Parallelism::None,
-    );
-
-    y
+    x
 }
