a0e608d38aa445b3b9d2ca0f958eea79826b1fd9
Daniel Duque <daniel.duque@cern.ch>
2023-07-14 00:45:50 -0700

    Implement pad non-negative greedy deconvolution

--- physics/src/pad_analysis.rs
+++ physics/src/pad_analysis.rs
@@ -24,3 +24,40 @@ lazy_static! {
             .collect()
     };
 }
+
+// There are too many pad signals to treat them in the same way as the anode
+// wires (too slow). We can take advantage of the fact that the response is
+// front-heavy, hence we can do a greedy deconvolution by just looking at the
+// few samples ahead of each time bin.
+// Given the details of our use-case add a non-negative constraint on the
+// deconvolved input (same as the anode wires).
+fn nn_greedy_deconvolution(
+    signal: &[f64],
+    response: &[f64],
+    offset: usize,
+    look_ahead: usize,
+    // Return the sum of residuals squared together with the reconstructed input
+) -> (f64, Vec<f64>) {
+    let mut residual = signal.to_vec();
+    let mut input = vec![0.0; signal.len()];
+
+    for i in 0..(residual.len() - offset - look_ahead) {
+        let val = residual[i + offset..][..look_ahead]
+            .iter()
+            .zip(response[offset..][..look_ahead].iter())
+            .map(|(s, r)| s / r)
+            .reduce(f64::min)
+            .unwrap();
+
+        if val > 0.0 {
+            input[i] = val;
+            residual[i..]
+                .iter_mut()
+                .zip(response)
+                .for_each(|(s, r)| *s -= val * r);
+        }
+    }
+    let residual = residual.iter().map(|x| x.powi(2)).sum();
+
+    (residual, input)
+}
