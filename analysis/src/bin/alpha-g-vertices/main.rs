9cc38f60d90e4a2c6c5aaa65fd2e8720296a2158
Daniel Duque <daniel.duque@cern.ch>
2023-12-31 02:15:24 -0800

    Write reconstructed vertices to csv file

--- analysis/src/bin/alpha-g-vertices/main.rs
+++ analysis/src/bin/alpha-g-vertices/main.rs
@@ -1,5 +1,5 @@
 use alpha_g_detector::midas::EventId;
-use alpha_g_physics::MainEvent;
+use alpha_g_physics::{MainEvent, TRG_CLOCK_FREQ};
 use anyhow::{ensure, Context, Result};
 use clap::Parser;
 use indicatif::{
@@ -7,25 +7,39 @@ use indicatif::{
 };
 use midasio::file::{initial_timestamp_unchecked, run_number_unchecked};
 use rayon::prelude::*;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path::PathBuf;
+use uom::si::length::meter;
+use uom::si::time::second;
 
 #[derive(Parser)]
 #[command(author, version)]
 #[command(about = "Reconstruct the annihilation vertices for a single run", long_about = None)]
 struct Args {
-    /// MIDAS files from the run you want to reconstruct.
+    /// MIDAS files from the run you want to reconstruct
     #[arg(required = true)]
     files: Vec<PathBuf>,
-    /// Ignore the first `SKIP` number of events.
+    /// Write the reconstructed vertices to `OUTPUT.csv`
+    #[arg(short, long)]
+    output: PathBuf,
+    /// Ignore the first `SKIP` number of events
     /// The first event not skipped sets `t=0`
     // The default here is used to skip the initial 10 synchronization software
     // triggers.
     #[arg(long, default_value = "10", verbatim_doc_comment)]
     skip: usize,
-    /// Print detailed information about errors (if any).
+    /// Print detailed information about errors (if any)
     #[arg(short, long)]
     verbose: bool,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct Row {
+    serial_number: u32,
+    trg_time: Option<f64>,
+    reconstructed_x: Option<f64>,
+    reconstructed_y: Option<f64>,
+    reconstructed_z: Option<f64>,
 }
 
 fn main() -> Result<()> {
@@ -97,6 +111,52 @@ fn main() -> Result<()> {
         );
         tp_bar.inc(1);
     }
+    tp_bar.finish_and_clear();
+
+    let rows = rows
+        .into_iter()
+        .skip(args.skip)
+        .scan(
+            (None, 0),
+            |(previous, cumulative), (serial_number, timestamp, vertex)| {
+                let current = timestamp.unwrap_or(previous.unwrap_or(0));
+                let delta = current.wrapping_sub(previous.unwrap_or(current));
+                *previous = Some(current);
+                *cumulative += u64::from(delta);
+
+                if timestamp.is_none() {
+                    Some((serial_number, None, None))
+                } else {
+                    Some((serial_number, Some(*cumulative), vertex))
+                }
+            },
+        )
+        .map(|(serial_number, cumulative, vertex)| Row {
+            serial_number,
+            trg_time: cumulative.map(|t| (t as f64 / TRG_CLOCK_FREQ).get::<second>()),
+            reconstructed_x: vertex.map(|v| v.x.get::<meter>()),
+            reconstructed_y: vertex.map(|v| v.y.get::<meter>()),
+            reconstructed_z: vertex.map(|v| v.z.get::<meter>()),
+        });
+
+    let output = args.output.with_extension("csv");
+    let mut wtr = std::fs::File::create(&output)
+        .with_context(|| format!("failed to create `{}`", output.display()))?;
+    eprintln!("Created `{}`", output.display());
+    wtr.write_all(
+        format!(
+            "# Version {}\n# {}\n",
+            env!("CARGO_PKG_VERSION"),
+            std::env::args().collect::<Vec<_>>().join(" ")
+        )
+        .as_bytes(),
+    )
+    .context("failed to write comment to csv")?;
+    let mut wtr = csv::Writer::from_writer(wtr);
+    for row in rows {
+        wtr.serialize(row).context("failed to write row to csv")?;
+    }
+    wtr.flush().context("failed to flush csv")?;
 
     Ok(())
 }
