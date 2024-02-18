dfd84eddbc02b978028433c44a58109587af6756
Daniel Duque <daniel.duque@cern.ch>
2024-02-18 10:57:06 -0800

    Unpack chronobox data packets

--- detector/src/chronobox.rs
+++ detector/src/chronobox.rs
@@ -1,4 +1,8 @@
 use thiserror::Error;
+use winnow::binary::{le_u32, length_repeat};
+use winnow::combinator::{alt, empty, repeat_till, seq};
+use winnow::error::{ContextError, StrContext};
+use winnow::{PResult, Parser};
 
 /// The error type returned when conversion from unsigned integer to
 /// [`ChannelId`] fails.
@@ -43,6 +47,31 @@ pub struct TimestampCounter {
     pub edge: EdgeType,
 }
 
+fn timestamp_counter(input: &mut &[u8]) -> PResult<TimestampCounter> {
+    let (channel, timestamp, edge) = le_u32
+        .context(StrContext::Label("timestamp counter word"))
+        .verify(|&word| word & 0x80000000 != 0)
+        .context(StrContext::Label("top bit"))
+        .try_map(|word| {
+            let channel = ChannelId::try_from((word >> 24) as u8 & 0x7F)?;
+            let timestamp = word & 0x00FFFFFE;
+            let edge = if word & 1 == 1 {
+                EdgeType::Trailing
+            } else {
+                EdgeType::Leading
+            };
+
+            Ok::<(_, _, _), TryChannelIdFromUnsignedError>((channel, timestamp, edge))
+        })
+        .parse_next(input)?;
+
+    Ok(TimestampCounter {
+        channel,
+        timestamp,
+        edge,
+    })
+}
+
 impl TimestampCounter {
     /// Returns the timestamp value. This counter is 24 bits wide, hence the
     /// most significant 8 bits are always zero.
@@ -65,6 +94,25 @@ pub struct WrapAroundMarker {
     counter: u32,
 }
 
+fn wrap_around_marker(input: &mut &[u8]) -> PResult<WrapAroundMarker> {
+    let (timestamp_top_bit, counter) = le_u32
+        .context(StrContext::Label("wrap around counter word"))
+        .verify(|&word| word & 0xFF000000 == 0xFF000000)
+        .context(StrContext::Label("top 8 bits"))
+        .map(|word| {
+            let timestamp_top_bit = word & 0x00800000 != 0;
+            let counter = word & 0x007FFFFF;
+
+            (timestamp_top_bit, counter)
+        })
+        .parse_next(input)?;
+
+    Ok(WrapAroundMarker {
+        timestamp_top_bit,
+        counter,
+    })
+}
+
 impl WrapAroundMarker {
     /// Returns the wrap around counter. This counter increments with every wrap
     /// around marker written to the Chronobox FIFO (i.e. 2 times per timestamp
@@ -84,6 +132,33 @@ pub enum FifoEntry {
     WrapAroundMarker(WrapAroundMarker),
 }
 
+/// The error type returned when conversion from
+/// [`&[u8]`](https://doc.rust-lang.org/std/primitive.slice.html) to
+/// [`ChronoPacket`] fails.
+#[derive(Debug)]
+pub struct TryChronoPacketFromBytesError {
+    offset: usize,
+    inner: ContextError,
+}
+
+impl std::fmt::Display for TryChronoPacketFromBytesError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "parsing stopped at byte offset `{}`", self.offset)?;
+        if self.inner.context().next().is_some() {
+            write!(f, " ({})", self.inner)?;
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for TryChronoPacketFromBytesError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.inner
+            .cause()
+            .map(|v| v as &(dyn std::error::Error + 'static))
+    }
+}
+
 /// Frequency (Hertz) of the system clock.
 pub const SYS_CLOCK_FREQ: f64 = 100e6;
 
@@ -97,6 +172,32 @@ pub struct ChronoPacket {
     /// System clock counter which increments at a frequency of
     /// [`SYS_CLOCK_FREQ`].
     pub sys_clock: u32,
+}
+
+fn chrono_packet(input: &mut &[u8]) -> PResult<ChronoPacket> {
+    seq! {ChronoPacket{
+        fifo: repeat_till(0.., alt((
+            timestamp_counter.map(FifoEntry::TimestampCounter),
+            wrap_around_marker.map(FifoEntry::WrapAroundMarker)
+        )), b"\x3C\x00\x00\xFE").map(|(fifo, _)| fifo),
+        scalers: length_repeat(empty.value(NUM_INPUT_CHANNELS), le_u32)
+            .map(|v: Vec<_>| v.try_into().unwrap()),
+        sys_clock: le_u32,
+    }}
+    .parse_next(input)
+}
+
+impl TryFrom<&[u8]> for ChronoPacket {
+    type Error = TryChronoPacketFromBytesError;
+
+    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
+        chrono_packet
+            .parse(bytes)
+            .map_err(|e| TryChronoPacketFromBytesError {
+                offset: e.offset(),
+                inner: e.into_inner(),
+            })
+    }
 }
 
 impl ChronoPacket {
--- detector/src/chronobox/tests.rs
+++ detector/src/chronobox/tests.rs
@@ -26,3 +26,171 @@ fn chronopacket_scaler() {
         assert_eq!(packet.scaler(channel_id), u32::from(num));
     }
 }
+
+fn timestamp_counter(channel: u8, timestamp: u32, edge: bool) -> u32 {
+    0x80000000 | (u32::from(channel) << 24) | (timestamp & 0x00FFFFFE) | u32::from(edge)
+}
+
+fn wrap_around_marker(top_bit: bool, counter: u32) -> u32 {
+    0xFF000000 | (u32::from(top_bit) << 23) | (counter & 0x007FFFFF)
+}
+
+const SCALERS_HEADER: u32 = 0xFE00003C;
+
+#[test]
+fn chronopacket_invalid_top_bit() {
+    let bad_tsc = timestamp_counter(0, 0, false) & 0x7FFFFFFF;
+    let bytes = [
+        &bad_tsc.to_le_bytes()[..],
+        &SCALERS_HEADER.to_le_bytes()[..],
+        &[0; 60 * 4],
+    ]
+    .concat();
+    assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+}
+
+#[test]
+fn chronopacket_invalid_channel() {
+    for channel in 59..=126 {
+        let bad_tsc = timestamp_counter(channel, 0, false);
+        let bytes = [
+            &bad_tsc.to_le_bytes()[..],
+            &SCALERS_HEADER.to_le_bytes()[..],
+            &[0; 60 * 4],
+        ]
+        .concat();
+        assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+    }
+}
+
+#[test]
+fn chronopacket_unknown_word() {
+    let tsc = timestamp_counter(0, 0, false);
+    let wam = wrap_around_marker(false, 0);
+    let unknown_word = 0xFC000000u32;
+    let bytes = [
+        &tsc.to_le_bytes()[..],
+        &wam.to_le_bytes()[..],
+        &unknown_word.to_le_bytes()[..],
+        &SCALERS_HEADER.to_le_bytes()[..],
+        &[0; 60 * 4],
+    ]
+    .concat();
+
+    assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+}
+
+#[test]
+fn chronopacket_missing_scalers() {
+    let bytes = Vec::new();
+    assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+}
+
+#[test]
+fn chronopacket_short_scalers() {
+    let bytes = [&SCALERS_HEADER.to_le_bytes()[..], &[0; 59 * 4]].concat();
+    assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+}
+
+#[test]
+fn chronopacket_long_scalers() {
+    let bytes = [&SCALERS_HEADER.to_le_bytes()[..], &[0; 61 * 4]].concat();
+    assert!(ChronoPacket::try_from(&bytes[..]).is_err());
+}
+
+#[test]
+fn chronopacket_good_single_timestamp_counter() {
+    let tsc = timestamp_counter(11, 0, false);
+    let bytes = [
+        &tsc.to_le_bytes()[..],
+        &SCALERS_HEADER.to_le_bytes()[..],
+        &[0; 60 * 4],
+    ]
+    .concat();
+
+    let packet = ChronoPacket::try_from(&bytes[..]).unwrap();
+    let ChronoPacket {
+        fifo,
+        scalers,
+        sys_clock,
+    } = packet;
+
+    assert!(fifo.len() == 1);
+    let FifoEntry::TimestampCounter(tsc) = fifo[0] else {
+        unreachable!();
+    };
+    assert_eq!(tsc.channel, ChannelId::try_from(11).unwrap());
+    assert_eq!(tsc.timestamp(), 0);
+    assert!(matches!(tsc.edge, EdgeType::Leading));
+    assert_eq!(scalers, [0; 59]);
+    assert_eq!(sys_clock, 0);
+}
+
+#[test]
+fn chronopacket_good_single_wrap_around_marker() {
+    let wam = wrap_around_marker(true, 0);
+    let bytes = [
+        &wam.to_le_bytes()[..],
+        &SCALERS_HEADER.to_le_bytes()[..],
+        &[0xFF; 60 * 4],
+    ]
+    .concat();
+
+    let packet = ChronoPacket::try_from(&bytes[..]).unwrap();
+    let ChronoPacket {
+        fifo,
+        scalers,
+        sys_clock,
+    } = packet;
+    assert!(fifo.len() == 1);
+    let FifoEntry::WrapAroundMarker(wam) = fifo[0] else {
+        unreachable!();
+    };
+    assert!(wam.timestamp_top_bit);
+    assert_eq!(wam.wrap_around_counter(), 0);
+    assert_eq!(scalers, [0xFFFFFFFF; 59]);
+    assert_eq!(sys_clock, 0xFFFFFFFF);
+}
+
+#[test]
+fn chronopacket_good_only_scalers() {
+    let bytes = [&SCALERS_HEADER.to_le_bytes()[..], &[0xFF; 60 * 4]].concat();
+    let packet = ChronoPacket::try_from(&bytes[..]).unwrap();
+    let ChronoPacket {
+        fifo,
+        scalers,
+        sys_clock,
+    } = packet;
+    assert!(fifo.is_empty());
+    assert_eq!(scalers, [0xFFFFFFFF; 59]);
+    assert_eq!(sys_clock, 0xFFFFFFFF);
+}
+
+#[test]
+fn chronopacket_good_full_packet() {
+    let mut bytes = Vec::new();
+    for i in 0..10 {
+        for channel in 0..=58 {
+            let tsc_le = timestamp_counter(channel, i, false);
+            bytes.extend_from_slice(&tsc_le.to_le_bytes()[..]);
+
+            let tsc_te = timestamp_counter(channel, i + 1, true);
+            bytes.extend_from_slice(&tsc_te.to_le_bytes()[..]);
+        }
+
+        let wam = wrap_around_marker(i % 2 == 0, i);
+        bytes.extend_from_slice(&wam.to_le_bytes()[..]);
+    }
+    bytes.extend_from_slice(&SCALERS_HEADER.to_le_bytes()[..]);
+    bytes.extend_from_slice(&[0x01; 60 * 4]);
+
+    let packet = ChronoPacket::try_from(&bytes[..]).unwrap();
+    let ChronoPacket {
+        fifo,
+        scalers,
+        sys_clock,
+    } = packet;
+    assert_eq!(fifo.len(), (59 * 2 + 1) * 10);
+    assert_eq!(scalers, [0x01010101; 59]);
+    assert_eq!(sys_clock, 0x01010101);
+}
